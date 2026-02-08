41e7874c903455cc44c722dea6152599739cf8cb
Suraj1o1 <86189592+Suraj1o1@users.noreply.github.com>
2026-02-08 13:47:47 +0545

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-
 import './App.css'
 import ValentinePage from './components/ValentinePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
